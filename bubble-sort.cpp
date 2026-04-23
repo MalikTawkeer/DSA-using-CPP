71e6cbb544d8958cb0ca87fd9079c697dc92624a
Malik towkeer <maliktow012@gmail.com>
2026-04-23 22:48:12 +0530

    Refactor bubble sort to improve efficiency by adding early exit condition

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -5,12 +5,19 @@ void bubbleSort(int arr[], int n)
 {
     for (int i = n - 1; i >= 1; i--)
     {
+        int didAnySwaps = 0;
         for (int j = 0; j <= i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                didAnySwaps = 1;
             }
+        }
+
+        if (!didAnySwaps)
+        {
+            break;
         }
     }
 }
