e43964b055cb753f1bb97b2cfceb9b79661aa5d8
Malik towkeer <maliktow012@gmail.com>
2026-04-23 22:41:59 +0530

    Add bubble sort implementation and update output file

---
+++ bubble-sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[6] = {13, 46, 24, 52, 20, 9};
+    int n = 6;
+
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
